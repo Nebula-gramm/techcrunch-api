a17ab49fdb3b3d46011c45b67f1e7a34ae9c5625
unknown <aijourneybd.blog@gamil.com>
2025-12-12 20:57:20 +0600

    for deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const path = require("path")
 const postRoute = require("./routes/postRoute")
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 
 //serve the build path
